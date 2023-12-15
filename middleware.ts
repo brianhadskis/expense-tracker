543d4402f3c97cb10641895e7abdad64d9ba48d9
Brian <brian.hadskis@hotmail.com>
2023-12-14 20:48:22 -0500

    fix: downgraded next-auth to fix issue with middleware and prisma - changed next-auth configuration to accomodate the change to next-auth v4.24.5

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,23 @@
-import { auth, authConfig } from "@/auth";
-import NextAuth from "next-auth";
+import { withAuth } from "next-auth/middleware";
+import { NAVIGATION } from "./config";
 
-export default NextAuth(authConfig).auth;
+export default withAuth({
+  callbacks: {
+    authorized({ req, token }) {
+      const isLoggedIn = !!token;
+      const protectedNav = NAVIGATION.filter((nav) => nav.loginRequired);
+      const isProtected = protectedNav.some((nav) =>
+        req.nextUrl.pathname.startsWith(nav.href)
+      );
+
+      if (isProtected && !isLoggedIn) {
+        return false;
+      }
+
+      return true;
+    },
+  },
+});
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
